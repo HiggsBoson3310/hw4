a1cb67c494f824c2b3a85990280e4b5ca330c8c7
HiggsBoson3310 <miguel.alarcon3310@gmail.com>
2017-04-17 13:53:36 -0500

    Add files via upload

--- Plots_Temperatura.py
+++ Plots_Temperatura.py
@@ -1,1 +1,17 @@
 #Este es python de las graficas
+#Importamos los paquetes necesarios para realizar las graficas
+import numpy as np
+import matplotlib.pyplot as plt
+#Leemos los datos y los guardamos en listas
+data1 = open('data1.dat', 'r')
+data1 = data1.readlines()
+data2 = open('data2.dat','r')
+data2 = data2.readlines()
+data3 = open('data3.dat','r')
+#Organizamos los datos de forma que nos queden en listas de forma que queden sin espacios
+d1 = []
+d2 = []
+d3 = []
+for i in range(data1):
+	d1.append(data1[i].split())
+
